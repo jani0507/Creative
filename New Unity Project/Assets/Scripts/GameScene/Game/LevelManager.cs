18cf02a4f59fd2d844265949915a3b5dd2d98faa
jani0507 <69594245+jani0507@users.noreply.github.com>
2020-08-15 17:54:43 +0200

    Segments

--- New Unity Project/Assets/Scripts/GameScene/Game/LevelManager.cs
+++ New Unity Project/Assets/Scripts/GameScene/Game/LevelManager.cs
@@ -1,20 +1,131 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance { set; get; }
 
+    private const bool SHOW_COLLIDER = true;
+
     //Level Spawning
+    private const float DISTANCE_BEFORE_SPAWN = 100.0f;
+    private const int INITIAL_SEGMENTS = 10;
+    private const int MAX_SEGMENTS_ON_SCREEN = 15;
+    private Transform cameraContainer;
+    private int amountOfActiveSegments;
+    private int continiousSegments;
+    private int currentSpawnZ;
+    private int currentLevel;
+    private int y1, y2, y3;
 
     //List of pieces
     public List<Piece> ramps = new List<Piece>();
     public List<Piece> longblocks = new List<Piece>();
     public List<Piece> jumps = new List<Piece>();
     public List<Piece> slides = new List<Piece>();
+    [HideInInspector]
     public List<Piece> pieces = new List<Piece>();
 
+    //List if Segments
+    public List<Segment> availableSegments = new List<Segment>();
+    public List<Segment> availableTransition = new List<Segment>();
+    public List<Segment> segments = new List<Segment>();
+
+    //Gameplay
+    private bool isMoving = false;
+
+    private void Awake()
+    {
+        Instance = this;
+        cameraContainer = Camera.main.transform;
+        currentSpawnZ = 0;
+        currentLevel = 0;
+    }
+    private void Start()
+    {
+        for (int i = 0; i < INITIAL_SEGMENTS; i++)
+            GenerateSegment();
+        
+    }
+
+    private void GenerateSegment()
+    {
+        SpawnSegment();
+
+        if(Random.Range(0f,1f) <(continiousSegments * 0.25f))
+        {
+            continiousSegments = 0;
+            SpawnTransition();
+        }
+        else
+        {
+            continiousSegments++;
+        }
+        
+    }
+
+    private void SpawnSegment()
+    {
+        List<Segment> possibleSeg = availableSegments.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleSeg.Count);
+
+        Segment s = GetSegment(id, false);
+
+        y1 = s.endY1;
+        y2 = s.endY2;
+        y3 = s.endY3;
+
+        s.transform.SetParent(transform);
+        s.transform.localPosition = Vector3.forward * currentSpawnZ;
+
+        currentSpawnZ += s.length;
+        amountOfActiveSegments++;
+        s.Spawn();
+    }
+    private void SpawnTransition()
+    {
+        List<Segment> possibleTransition = availableTransition.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleTransition.Count);
+
+        Segment s = GetSegment(id, true);
+
+        y1 = s.endY1;
+        y2 = s.endY2;
+        y3 = s.endY3;
+
+        s.transform.SetParent(transform);
+        s.transform.localPosition = Vector3.forward * currentSpawnZ;
+
+        currentSpawnZ += s.length;
+        amountOfActiveSegments++;
+        s.Spawn();
+    }
+
+    public Segment GetSegment(int id, bool transition)
+    {
+        Segment s = null;
+        s = segments.Find(x => x.SegId == id && x.transition == transition && !x.gameObject.activeSelf);
+
+        if(s == null)
+        {
+            GameObject go = Instantiate((transition) ? availableTransition[id].gameObject : availableSegments[id].gameObject) as GameObject;
+            s = go.GetComponent<Segment>();
+
+            s.SegId = id;
+            s.transition = transition;
+
+            segments.Insert(0, s);
+        }
+        else
+        {
+            segments.Remove(s);
+            segments.Insert(0, s);
+        }
+
+        return s;
+    }
     public Piece GetPiece(PieceTyp pt, int visualIndex)
     {
         Piece p = pieces.Find(x => x.type == pt && x.visualsIndex == visualIndex && !x.gameObject.activeSelf);
